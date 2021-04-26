6d8fae55031671583a2a4141ab69b7722db58201
15git-ruimendes <60974905+15git-ruimendes@users.noreply.github.com>
2021-04-26 15:59:08 +0100

    Add files via upload

---
+++ GreenNet_v2/GreenNet/Clustering.cpp
@@ -0,0 +1,219 @@
+#include "Clustering.h"
+
+#define TempValD 10
+#define TempValF 0.5
+
+
+void Clusters::OptimizeClusters(std::vector<Local> Armazens, std::vector<Local> Lojas, float sigma)
+{
+	clock_t init;
+	double diff = 0,rho,Temp;
+	unsigned int Iter = 0;
+	srand((unsigned)std::time(0));
+
+	init = clock();
+	CreatFirstSolution(Armazens, Lojas);
+	double BestVal = EvaluateSolution('C');
+	std::cout << "Valor de Avaliacao:   ";
+	std::cout << BestVal << std::endl;
+	printClusterCurrent('C');
+	printf("=================================== %d\n", ClusterCurrent.size());
+	ClusterBest = ClusterCurrent;
+	while ((clock() - init)/CLOCKS_PER_SEC < 300 && Iter < 4000000 ) {
+		
+		CreatNeighborhood();
+
+		Temp = exp(-sigma * (clock() - init));
+
+		if (EvaluateSolution('P') < BestVal) {
+			BestVal = EvaluateSolution('P');
+			ClusterBest = ProposedCluster;
+			Iter = 0;
+			std::cout << "Distnacia Total:   ";
+			std::cout << BestVal << std::endl;
+			printClusterCurrent('B');
+			printf("=================================== %d\n",ClusterBest.size());
+
+		}
+		Iter++;
+		diff = EvaluateSolution('P') - EvaluateSolution('C');
+		rho = exp(-diff / Temp);
+		if (rand() % 1 < rho)
+		{
+			ClusterCurrent = ProposedCluster;
+			ProposedCluster.clear();
+		}
+		else 
+			ProposedCluster.clear();
+	}
+}
+
+void Clusters::printClusterCurrent(char Qual)
+{
+	if (Qual == 'C') 
+	{
+		std::cout << "ClusterCurrent Being Printed! \n";
+		for (int i = 0; i < ClusterCurrent.size(); i++) {
+			for (int j = 0; j < ClusterCurrent[i].size(); j++) {
+				std::cout << ClusterCurrent[i][j].Id << "\t";
+			}
+			std::cout << std::endl;
+		}
+	}
+	else if (Qual == 'P') 
+	{
+		std::cout << "ProposedCluster Being Printed! \n";
+		for (int i = 0; i < ProposedCluster.size(); i++) {
+			for (int j = 0; j < ProposedCluster[i].size(); j++) {
+				std::cout << ProposedCluster[i][j].Id << "\t";
+			}
+			std::cout << std::endl;
+		}
+	}
+	else if (Qual == 'B') 
+	{
+		std::cout << "ClusterBest Being Printed! \n";
+		for (int i = 0; i < ClusterBest.size(); i++) {
+			for (int j = 0; j < ClusterBest[i].size(); j++) {
+				std::cout << ClusterBest[i][j].Id << "\t";
+			}
+			std::cout << std::endl;
+		}
+	}
+}
+
+void Clusters::CreatNeighborhood()
+{
+	int A1 = 0, A2 = 0, LOg1 = 0, LOg2 = 0, Pos1 = 0, Pos2 = 0;
+	std::vector<Local>::iterator I1, I2;
+	do {
+		srand((unsigned)std::time(0));
+		A1 = rand() % (ClusterCurrent.size());
+		A2 = rand() % (ClusterCurrent.size());
+		Pos1 = rand() % (ClusterCurrent[A1].size() - 1) + 1;
+		Pos2 = rand() % (ClusterCurrent[A2].size() - 1) + 1;
+		if ((ClusterCurrent[A1].size() - 1 > 0) && (ClusterCurrent[A2].size() - 1 > 0))
+		{
+			LOg1 = rand() % (ClusterCurrent[A1].size() - 1) + 1;
+			LOg2 = rand() % (ClusterCurrent[A2].size() - 1) + 1;
+		}
+		else {
+			LOg1 = 0;
+			LOg2 = 0;
+		}
+	} while (A1 == A2 || LOg1 == 0 || LOg2 == 0 || Pos1 == Pos2);
+	ProposedCluster = ClusterCurrent;
+	//std::cout << Pos1 << "\t" << Pos2 << "\t" << A1 << "\t" << A2 << std::endl;
+	I1 = ProposedCluster[A1].begin() + Pos1;
+	I2 = ProposedCluster[A2].begin() + Pos2;
+	
+	//ProposedCluster[A1].resize(ProposedCluster[A1].size() + 1);
+	//ProposedCluster[A2].resize(ProposedCluster[A2].size() + 1);
+
+	ProposedCluster[A2].insert(I2,ProposedCluster[A1][LOg1]);
+	ProposedCluster[A1].insert(I1,ProposedCluster[A2][LOg2]);
+
+	ProposedCluster[A1].erase(ProposedCluster[A1].begin() + LOg1);
+	ProposedCluster[A2].erase(ProposedCluster[A2].begin() + LOg2);
+	
+	return;
+}
+
+
+
+void Clusters::CreatFirstSolution(std::vector<Local> Armazens, std::vector<Local> Lojas)
+{
+	long double xXArm, yYArm, Sum = 0, MinTempDist = 1000000, TempDist;
+	int IdMin = 0;
+	for (int i = 0; i < Armazens.size(); i++) {
+		xXArm = Armazens[i].xX;
+		yYArm = Armazens[i].xX;
+		ClusterCurrent.resize(i+1);
+		ClusterCurrent[i].push_back(Armazens[i]);
+		if (Lojas.size() != 0) {
+			while (Armazens[i].Stocks >= Sum) {
+				for (int j = 0; j < Lojas.size(); j++) {
+					TempDist = DetermineDistance(xXArm, Lojas[j].xX, yYArm, Lojas[j].yY);
+					if (TempDist <= MinTempDist) {
+						MinTempDist = TempDist;
+						IdMin = j;
+					}
+				}
+				
+				MinTempDist = 10000000;
+				if (Lojas.size() == 0) {
+					break;
+				}
+				Sum += Lojas[IdMin].Stocks;
+
+				if (Sum >= Armazens[i].Stocks) {
+					Sum = 0;
+					break;
+				}
+				ClusterCurrent[i].push_back(Lojas[IdMin]);
+				Lojas.erase(Lojas.begin() + (IdMin));
+			}
+		}
+	}
+}
+
+double Clusters::EvaluateSolution(char Qual)
+{
+	double TotDist = 0, Entrgue = 0;
+	if (Qual == 'C') 
+	{
+		for (int i = 0; i < ClusterCurrent.size(); i++)
+		{
+			TotDist += CalculateFitness(ClusterCurrent[i]);
+		
+		}
+		return TotDist;
+	}
+	if (Qual == 'P')
+	{
+		for (int i = 0; i < ProposedCluster.size(); i++)
+		{
+			TotDist += CalculateFitness(ProposedCluster[i]);
+		}
+		return TotDist;
+	}
+	if (Qual == 'B')
+	{
+		for (int i = 0; i < ClusterBest.size(); i++)
+		{
+			TotDist += CalculateFitness(ClusterBest[i]);
+		}
+		return TotDist;
+	}
+}
+
+double Clusters::DetermineDistance(long double x1, long double x2, long double y1, long double y2)
+{
+	return sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+}
+double Clusters::CalculateFitness(std::vector<Local> Prop)
+{
+	double aux = 0,StockCheck = 0;
+	int id[86] = { 0 };
+
+	for (int i = 0; i < Prop.size() - 1; i++)
+	{
+		aux += DetermineDistance(Prop[i].xX, Prop[i + 1].xX, Prop[i].yY, Prop[i + 1].yY);
+	}
+	aux += DetermineDistance(Prop[Prop.size() - 1].xX, Prop[0].xX, Prop[Prop.size() - 1].yY, Prop[0].yY);
+	for (int i = 1; i < Prop.size(); i++) 
+	{
+		id[i] = Prop[i].Id;
+		StockCheck += Prop[i].Stocks;
+		if (StockCheck > Prop[0].Stocks)
+			return 1000000;
+		for (int j = 1; j < i; j++) 
+		{
+			if (id[i] == id[j])
+				return 1000000;
+		}
+	}
+
+	return aux;
+}
+
---
+++ GreenNet_v2/GreenNet/GreenNet.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#include "Clustering.h"
+#include "TSM.h"
+
+#define NumArmz 100
+#define NumLojas 200
+#define NumParam 4
+
+double Distance(long double x1, long double x2, long double y1, long double y2)
+{
+	return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+}
+
+double CheckPathDistance(std::vector<Local> Path) {
+	double Dist = 0;
+	for (int i = 0; i < Path.size() - 1; i++) {
+		Dist += Distance(Path[i].xX, Path[i + 1].xX, Path[i].yY, Path[i + 1].yY);
+	}
+	Dist += Distance(Path[Path.size() - 1].xX, Path[0].xX, Path[Path.size() - 1].yY, Path[0].yY);
+	return Dist;
+}
+
+double CheckStock(std::vector<Local> Path) {
+	double Entregue = 0;
+	for (int i = 1; i < Path.size(); i++)
+	{
+		Entregue += Path[i].Stocks;
+	}
+	return Entregue;
+}
+
+void FinalStockCheck(std::vector<std::vector<Local>> BestClust,int *checkstock,float *maxdist1,int *lojasAbas,float *avgFullFill) 
+{
+	
+	(*checkstock) = 1;
+	long double stocks, maxdist=0, maxdistaux=0,fullfillaux=0;
+	for (int i = 0; i < BestClust.size() ; i++)
+	{
+		stocks = BestClust[i][0].Stocks;
+		maxdist += CheckPathDistance(BestClust[i]);
+		for (int j = 1; j < BestClust[i].size(); j++) 
+		{
+			stocks -= BestClust[i][j].Stocks;
+			if (stocks < 0)
+				(*checkstock) = 0;
+		}
+
+		fullfillaux += 1 - (stocks) / BestClust[i][0].Stocks;
+		(*lojasAbas) += (BestClust[i].size()-1);
+	}
+	(*maxdist1) = (float) maxdist;
+	(*avgFullFill) = (float)(fullfillaux /( BestClust.size()));
+}
+
+
+
+class Parse
+{
+public:
+	void ParseFile(std::vector<std::string> Array, std::string Delim,std::vector<std::vector<long double>>& Res,int Size) {
+			
+		for (int i = 0; i < Size;i++) {
+			Res.resize(i + 1);
+			size_t pos = 0;
+			std::string token;
+			int j = 0;
+			while ((pos = Array[i].find(Delim)) != std::string::npos) {
+				token = Array[i].substr(0, pos);
+				
+				if (i >= 1) {
+					(Res)[i - 1].push_back(std::stold(token));
+				}
+				j++;
+				Array[i].erase(0, pos + Delim.length());
+			}
+			if (i >= 1) {
+				(Res)[i - 1].push_back(std::stold(Array[i]));
+			}
+			
+		}
+	}
+};
+
+
+
+
+int main() {
+	std::cout << "Running ! \n";
+
+	Parse p;
+
+	int TexArm = 0, TexLoj = 0;
+
+	std::vector<std::string> ArmazensTxt(NumArmz);
+	std::fstream ArmzFile("Dados/Armazens.txt");
+	for (int i = 0; std::getline(ArmzFile, ArmazensTxt[i]);i++)
+	{
+		TexArm++;
+		//std::cout << ArmazensTxt[i];
+		//std::cout << "\n";
+	}
+	std::vector<std::string> LojasTxt(NumLojas);
+	std::fstream LojFile("Dados/Lojas.txt");
+	for (int i = 0; std::getline(LojFile, LojasTxt[i]);i++)
+	{
+		TexLoj++;
+		//std::cout << LojasTxt[i];
+		//std::cout << "\n";
+	}
+	std::cout << "Files Read \n";
+
+	std::vector<std::vector<long double>> ArmazensSplit;
+	std::vector<std::vector<long double>> LojasSplit;
+	p.ParseFile(ArmazensTxt, ";", ArmazensSplit, TexArm);
+	p.ParseFile(LojasTxt, ";", LojasSplit, TexLoj);
+	std::cout << "Files Parsed \n";
+
+
+	std::vector<Local> ArmazemVect;
+	std::vector<Local> LojaVect;
+	for (int i = 0; i < ArmazensSplit.size() - 1;i++) {
+		Local aux(ArmazensSplit[i], 'A');
+		ArmazemVect.push_back(aux);
+	}
+	for (int i = 0; i < LojasSplit.size() - 1; i++) {
+		Local aux(LojasSplit[i], 'L');
+		LojaVect.push_back(aux);
+	}
+	std::cout << "Local Matrixes Done\n";
+
+	Clusters Clust;
+	printf("Lojas Iniciais %d\n", LojaVect.size());
+	Clust.OptimizeClusters(ArmazemVect, LojaVect, 0.99);
+	//Clust.printClusterCurrent('B');
+
+	int checkstock = 0,lojasAbst=0;
+	float maxdist = 0,avgFulFill =0;
+	FinalStockCheck(Clust.ClusterBest,&checkstock,&maxdist,&lojasAbst,&avgFulFill);
+	printf("Are clusters compatible with stocks: %d  ; Total Distance:  %f; Fullfiled Shops:  %d; Avg Usage :  %f %%\n", checkstock, maxdist,lojasAbst,(avgFulFill*100));
+
+	std::cout << "===================================\n";
+	std::cout << "Final Paths Being Printed! \n";
+	for (int i = 0; i < Clust.ClusterBest.size(); i++) {
+		for (int j = 0; j < 10; j++) {
+			if (j < Clust.ClusterBest[i].size())
+				std::cout << Clust.ClusterBest[i][j].Id << "\t";
+			else
+				std::cout << "\t";
+		}
+		std::cout << CheckPathDistance(Clust.ClusterBest[i]) << "   Capacity :" << Clust.ClusterBest[i][0].Stocks << "   Delivered:  " << CheckStock(Clust.ClusterBest[i])<<std::endl;
+	}
+
+	return 0;
+}
